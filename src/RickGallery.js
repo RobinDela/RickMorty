e6f5941ec2f02c437e152dfc929fefde976dfca5
Zamomako <robin-delannoy_student2021@wilder.school>
2021-05-07 16:54:03 +0200

    Add Axios call to get Char name and pictures

--- src/RickGallery.js
+++ src/RickGallery.js
@@ -1,15 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './RickGallery.css';
 
 function RickGallery() {
-  const [characters] = useState(['Mister Meeseek', 'PoopyHole']);
+  const [characters, setCharacters] = useState(['Mister Meeseek', 'PoopyHole']);
+  useEffect(
+    () =>
+      axios
+        .get('https://rickandmortyapi.com/api/character')
+        .then(({ data }) => {
+          console.log(data.results[0]);
+          setCharacters(data.results);
+        }),
+    []
+  );
+
   return (
     <>
       <h2>Personnages</h2>
       <ul className='charList'>
-        {characters.map((character) => {
-          return <li>{character}</li>;
-        })}
+        <div className='Gallery'>
+          {characters.map((character) => {
+            return (
+              <li>
+                <h3>{character.name}</h3>
+                <img src={character.image} alt='' />
+              </li>
+            );
+          })}
+        </div>
       </ul>
     </>
   );
